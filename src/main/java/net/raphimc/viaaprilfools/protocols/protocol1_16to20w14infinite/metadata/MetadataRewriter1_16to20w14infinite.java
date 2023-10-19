3afef18de616dee536bbfc4befcb8928afa4bf70
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-10-19 12:09:18 +0200

    Updated Via API usage

--- src/main/java/net/raphimc/viaaprilfools/protocols/protocol1_16to20w14infinite/metadata/MetadataRewriter1_16to20w14infinite.java
+++ src/main/java/net/raphimc/viaaprilfools/protocols/protocol1_16to20w14infinite/metadata/MetadataRewriter1_16to20w14infinite.java
@@ -18,11 +18,11 @@
 package net.raphimc.viaaprilfools.protocols.protocol1_16to20w14infinite.metadata;
 
 import com.viaversion.viaversion.api.connection.UserConnection;
-import com.viaversion.viaversion.api.minecraft.entities.Entity1_16Types;
 import com.viaversion.viaversion.api.minecraft.entities.EntityType;
+import com.viaversion.viaversion.api.minecraft.entities.EntityTypes1_16;
 import com.viaversion.viaversion.api.minecraft.metadata.Metadata;
-import com.viaversion.viaversion.api.minecraft.metadata.types.MetaType1_14;
 import com.viaversion.viaversion.api.type.types.Particle;
+import com.viaversion.viaversion.api.type.types.version.Types1_14;
 import com.viaversion.viaversion.rewriter.EntityRewriter;
 import net.raphimc.viaaprilfools.protocols.protocol1_16to20w14infinite.ClientboundPackets20w14infinite;
 import net.raphimc.viaaprilfools.protocols.protocol1_16to20w14infinite.Protocol1_16to20w14infinite;
@@ -33,36 +33,36 @@ public class MetadataRewriter1_16to20w14infinite extends EntityRewriter<Clientbo
 
     public MetadataRewriter1_16to20w14infinite(Protocol1_16to20w14infinite protocol) {
         super(protocol);
-        mapEntityType(Entity20w14infiniteTypes.ZOMBIE_PIGMAN, Entity1_16Types.ZOMBIFIED_PIGLIN);
-        mapTypes(Entity20w14infiniteTypes.values(), Entity1_16Types.class);
+        mapEntityType(Entity20w14infiniteTypes.ZOMBIE_PIGMAN, EntityTypes1_16.ZOMBIFIED_PIGLIN);
+        mapTypes(Entity20w14infiniteTypes.values(), EntityTypes1_16.class);
     }
 
     @Override
     public EntityType typeFromId(int type) {
-        return Entity1_16Types.getTypeFromId(type);
+        return EntityTypes1_16.getTypeFromId(type);
     }
 
     @Override
     public void handleMetadata(int entityId, EntityType type, Metadata metadata, List<Metadata> metadatas, UserConnection connection) {
-        if (metadata.metaType() == MetaType1_14.Slot) {
+        if (metadata.metaType() == Types1_14.META_TYPES.itemType) {
             metadata.setValue(this.protocol.getItemRewriter().handleItemToClient(metadata.value()));
-        } else if (metadata.metaType() == MetaType1_14.BlockID) {
+        } else if (metadata.metaType() == Types1_14.META_TYPES.blockStateType) {
             int data = (int) metadata.getValue();
             metadata.setValue(protocol.getMappingData().getNewBlockStateId(data));
-        } else if (metadata.metaType() == MetaType1_14.PARTICLE) {
+        } else if (metadata.metaType() == Types1_14.META_TYPES.particleType) {
             rewriteParticle((Particle) metadata.getValue());
         }
 
         if (type == null) return;
 
-        if (type.isOrHasParent(Entity1_16Types.MINECART_ABSTRACT)
+        if (type.isOrHasParent(EntityTypes1_16.MINECART_ABSTRACT)
                 && metadata.id() == 10) {
             // Convert to new block id
             int data = (int) metadata.getValue();
             metadata.setValue(protocol.getMappingData().getNewBlockStateId(data));
         }
 
-        if (type.isOrHasParent(Entity1_16Types.ABSTRACT_ARROW)) {
+        if (type.isOrHasParent(EntityTypes1_16.ABSTRACT_ARROW)) {
             if (metadata.id() == 8) {
                 metadatas.remove(metadata);
             } else if (metadata.id() > 8) {

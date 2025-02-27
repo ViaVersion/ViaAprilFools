2034a1e00279243b2cede80f5647332ed9cc4690
FlorianMichael <florian.michael07@gmail.com>
2025-02-27 21:01:03 +0100

    Use 1.16 as base protocol determination for the infinite snapshot
    Related to https://github.com/ViaVersion/ViaVersion/pull/4400

--- common/src/main/java/com/viaversion/viaaprilfools/api/AprilFoolsProtocolVersion.java
+++ common/src/main/java/com/viaversion/viaaprilfools/api/AprilFoolsProtocolVersion.java
@@ -32,8 +32,8 @@ public class AprilFoolsProtocolVersion {
     public static final List<ProtocolVersion> SNAPSHOTS_PROTOCOLS = new ArrayList<>();
     public static final List<ProtocolVersion> APRIL_FOOLS_PROTOCOLS = new ArrayList<>();
 
-    public static final ProtocolVersion s3d_shareware = registerAprilFools(1, "3D Shareware", ProtocolVersion.v1_13_2);
-    public static final ProtocolVersion s20w14infinite = registerAprilFools(709, "20w14infinite", ProtocolVersion.v1_15_2);
+    public static final ProtocolVersion s3d_shareware = registerAprilFools(1, "3D Shareware", ProtocolVersion.v1_13_2, ProtocolVersion.v1_13_2);
+    public static final ProtocolVersion s20w14infinite = registerAprilFools(709, "20w14infinite", ProtocolVersion.v1_15_2, ProtocolVersion.v1_16);
     public static final ProtocolVersion sCombatTest8c = registerSnapshot(803, "Combat Test 8c", ProtocolVersion.v1_16_1);
 
     private static ProtocolVersion registerSnapshot(final int version, final String name, final ProtocolVersion origin) {
@@ -44,8 +44,16 @@ public class AprilFoolsProtocolVersion {
         return protocolVersion;
     }
 
-    private static ProtocolVersion registerAprilFools(final int version, final String name, final ProtocolVersion origin) {
-        final ProtocolVersion protocolVersion = new RedirectProtocolVersion(version, name, origin);
+    private static ProtocolVersion registerAprilFools(final int version, final String name, final ProtocolVersion origin, final ProtocolVersion baseProtocolVersion) {
+        final ProtocolVersion protocolVersion = new RedirectProtocolVersion(version, name, origin) {
+
+            @Override
+            public ProtocolVersion getBaseProtocolVersion() {
+                // The infinite snapshot needs to be treated as >=1.16 and not as 1.15.2 to have the correct uuid reading behavior in the base protocol
+                return baseProtocolVersion;
+            }
+
+        };
         ProtocolVersion.register(protocolVersion);
         PROTOCOLS.add(protocolVersion);
         APRIL_FOOLS_PROTOCOLS.add(protocolVersion);

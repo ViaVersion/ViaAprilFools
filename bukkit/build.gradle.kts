e7ee4ee38e40d39b82c9fe6012445ce4ef4dcbbf
FlorianMichael <florian.michael07@gmail.com>
2025-05-15 15:29:05 +0200

    Rewrite build files

---
+++ bukkit/build.gradle.kts
@@ -0,0 +1,4 @@
+dependencies {
+    compileOnly(project(":viaaprilfools-common"))
+    compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
+}
---
+++ fabric/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    compileOnly(project(":viaaprilfools-common"))
+    compileOnly("net.fabricmc:fabric-loader:0.11.3")
+    compileOnly("org.apache.logging.log4j:log4j-api:2.17.1")
+}
